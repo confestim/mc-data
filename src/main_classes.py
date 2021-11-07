68fbc08f62ac2d4c41e2154898b6f8817ab71d24
Boyan <boyan@bobokara.com>
2021-11-07 18:47:39 +0200

    started working on methods that give you specific data

--- src/main_classes.py
+++ src/main_classes.py
@@ -1,60 +1,99 @@
 import json
 import os
 import requests
-
+import logging 
+   
+logger_format = '%(asctime)s:%(threadName)s:%(message)s'
+logging.basicConfig(format=logger_format, level=logging.INFO, datefmt="%H:%M:%S")
 
 
 class Stats():
-
-    def __init__(self, path=os.path.abspath("../stats")):
+    """Gets all/user data from files
+       
+        Parameters
+        ----------
+        ```
+        path: Specific path of the 'stats' folder, defaults to '../stats'.
+ 
+        user: Name of user, defaults to None => all users.
+        ```
+        Returns
+        -------
+        ```
+        player_data: dict with the player(s) data.   
         
-        player_data, player_names = {}, []
-    
-        for i in os.listdir(path):
+        player_names: list with the player name(s).
+        ```
 
-            if ".json" in i:
-                with open(f"{path}/{i}", "r") as f:
-                    
-                    player = json.load(f)
-                    player_name = i.strip(".json")
-                    player_name = requests.get(f"https://api.minetools.eu/uuid/{player_name}").json()["name"]
-                    player_names.append(player_name)
-                    player_data[f"{player_name}"] = player
+    """
+    def __init__(self, path=os.path.abspath("../stats"), user=None):
+
+        try:
+            for i in os.listdir(path):
+                if "-" in i:
+                    new_name = i.replace("-", "")
+                    os.rename("{0}/{1}".format(path, i), "{0}/{1}".format(path, new_name))
+                    logging.info(f"File format of {i} corrected.")        
+
+        except Exception as e:
+            logging.info(e)
+            logging.info("This usually means that your files are already corrected.")
+
+
+        if user:
+            uuid = requests.get(f"https://api.minetools.eu/uuid/{user}").json()["id"]
+            with open("{0}/{1}.json".format(path, uuid), "r") as f:
+                player = json.load(f)
+            self.player_data = {user:player}
+            self.player_names = user
         
-            else:
-                print(f"Non json file - {i}\nSkipping...")
-
-        self.player_data = player_data
-        self.player_names = player_names
+        else:
+            player_data, player_names = {}, []
         
-    def top(self, name, limit=10) -> dict:
-        """This returns the top(10) blocks that a given user has mined"""
-        data = self.player_data
+            for i in os.listdir(path):
 
-        if not name in data.keys():
-            raise NameError(f"No '{name}' in the files. Try changing the config.")
-        
-        result = {}
-
-        all_data = data[name]["stats"]
-
-        for a in all_data: 
+                if ".json" in i:
+                    with open(f"{path}/{i}", "r") as f:
+                        
+                        player = json.load(f)
+                        player_name = i.strip(".json")
+                        player_name = requests.get(f"https://api.minetools.eu/uuid/{player_name}").json()["name"]
+                        player_names.append(player_name)
+                        player_data[f"{player_name}"] = player
             
-            sorted_list = dict(sorted(all_data[a].items(),key=lambda x:x[1], reverse=True))
-            
-            counter = 0
-            
-            for i in sorted_list.keys():
-                if counter >= limit:
-                    break
-                # result[i][a][counter] = sorted_list[i]
-                result[a] = {i:{}}
-                result[a][i] = {counter:sorted_list[i]}     
-                # result[i][a][counter] = sorted_list[i]
-                counter += 1
+                else:
+                    print(f"Non json file - {i}\nSkipping...")
 
-        return result
+            self.player_data = player_data
+            self.player_names = player_names
 
-a = Stats()
-user = a.top("turbox123")
-print(user)
+    def mined(self, data=None):
+        """Gets all blocks mined for the player(s) given
+        
+        Parameters
+        ----------
+
+        data : player data list with json/dict, defaults to None, which leads to using the data for the user(s) given when initializing the class.
+        
+        Returns
+        -------
+
+        mined_blocks : dict with all the mined blocks and their frequency
+        """
+
+        if not data:
+            data = self.player_data
+        mined_blocks = []
+
+
+        for i in data:
+            mined_blocks.append({i: data[i]["stats"]["minecraft:mined"]})
+        
+        return mined_blocks
+
+
+
+a = Stats=()
+
+mined = a.mined()
+print(mined)
